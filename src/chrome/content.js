f04039d095dbd7c742b4dce9224b135b1b6b7673
AshreneRoy <agr97g@gmail.com>
2021-06-11 21:02:47 +0400

    fix(indent)

--- src/chrome/content.js
+++ src/chrome/content.js
@@ -12,17 +12,17 @@ ReactDOM.render(<Canvas/>, app);
 app.style.display = 'none';
 
 chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		if( request.message === 'clicked_browser_action') {
-			toggle();
-		}
-	}
+  function(request) {
+    if( request.message === 'clicked_browser_action') {
+      toggle();
+    }
+  }
 );
 
-function toggle(){
-	if(app.style.display === 'none'){
-		app.style.display = 'block';
-	}else{
-		app.style.display = 'none';
-	}
-}
+const toggle = () => {
+  if(app.style.display === 'none'){
+    app.style.display = 'block';
+  }else{
+    app.style.display = 'none';
+  }
+};
